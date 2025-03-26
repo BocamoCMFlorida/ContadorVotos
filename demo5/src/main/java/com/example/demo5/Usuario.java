c9a2e14c7f320fbd6db6f36c50313b25806cf697
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-26 11:47:31 +0100

    atributos Usuario

--- demo5/src/main/java/com/example/demo5/Usuario.java
+++ demo5/src/main/java/com/example/demo5/Usuario.java
@@ -1,5 +1,11 @@
 package com.example.demo5;
 
 public class Usuario {
+private String DNI;
+private String contraseña;
+private String nombre;
+private int edad;
+private String sexo;
+private boolean HaVotado;
 
 }
