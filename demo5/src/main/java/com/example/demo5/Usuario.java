43b7aff2c6baf62dc7bc3ece582db0f42982cab5
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-26 11:51:36 +0100

    getters and setters

--- demo5/src/main/java/com/example/demo5/Usuario.java
+++ demo5/src/main/java/com/example/demo5/Usuario.java
@@ -8,4 +8,51 @@ private int edad;
 private String sexo;
 private boolean HaVotado;
 
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public boolean isHaVotado() {
+        return HaVotado;
+    }
+
+    public void setHaVotado(boolean haVotado) {
+        HaVotado = haVotado;
+    }
 }
