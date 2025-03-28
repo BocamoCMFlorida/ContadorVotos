297002b1d950796451b1c23f753e5336da0e1fec
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-28 12:17:03 +0100

    metodo cerrar sesion

--- demo5/src/main/java/com/example/demo5/UsuarioSesion.java
+++ demo5/src/main/java/com/example/demo5/UsuarioSesion.java
@@ -5,18 +5,18 @@ public class UsuarioSesion {
     // Variable estática para almacenar el DNI del usuario actual
     private static String dniActual;
 
-    // Método para establecer el DNI del usuario autenticado
     public static void setDniActual(String dni) {
         dniActual = dni;
     }
 
-    // Método para obtener el DNI del usuario actual
     public static String getDniActual() {
         return dniActual;
     }
 
-    // Método para verificar si el usuario está autenticado
     public static boolean isUsuarioAutenticado() {
         return dniActual != null && !dniActual.isEmpty();
+    }
+    public static void cerrarSesion() {
+        dniActual = null;
     }
 }
