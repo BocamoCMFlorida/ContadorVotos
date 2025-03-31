7995b927e33d6b2e3bffd75ce646a5bdca6f95d7
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-31 11:16:41 +0100

    cargar imagen

--- demo5/src/main/java/com/example/demo5/HelloApplication.java
+++ demo5/src/main/java/com/example/demo5/HelloApplication.java
@@ -12,9 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 640);
         stage.setTitle("Contador Votos");
-        stage.getIcons().add(new Image("file:C:/Users/borji/Desktop/ContarVotos/ContadorVotos/demo5/src/main/images/logo1.png"));
+        stage.getIcons().add(new Image(getClass().getResource("/com/example/demo5/logo.png").toExternalForm()));
         stage.setScene(scene);
         stage.show();
     }
