1c09b726e65e54b081303e499e4452432ef66406
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-04-02 12:53:17 +0200

    constructor Admin

--- demo5/src/main/java/com/example/demo5/Admin.java
+++ demo5/src/main/java/com/example/demo5/Admin.java
@@ -1,4 +1,9 @@
 package com.example.demo5;
 
-public class Admin {
+public class Admin extends Usuario{
+
+    public Admin(String DNI, String contraseña, String nombre, int edad, String sexo, boolean haVotado) {
+        super(DNI, contraseña, nombre, edad, sexo, haVotado);
+    }
+    
 }
