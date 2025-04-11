04dd0e2611e8b7ff13b1989d167e874d1f12e798
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-04-11 13:27:41 +0200

    del com

--- demo5/src/main/java/com/example/demo5/PartidoModel.java
+++ demo5/src/main/java/com/example/demo5/PartidoModel.java
@@ -10,17 +10,17 @@ public class PartidoModel extends DBUtil {
     public ArrayList<Partido> getPartidos() {
         ArrayList<Partido> resultado = new ArrayList<>();
         try {
-            // Aquí colocamos una consulta SQL adecuada para recuperar los partidos
+
             PreparedStatement ps = this.getConexion().prepareStatement("SELECT nombre, votos, siglas FROM partido");
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
-                // Extraemos los datos del ResultSet
+
                 String nombre = rs.getString("nombre");
                 int votos = rs.getInt("votos");
                 String siglas = rs.getString("siglas");
 
-                // Creamos el objeto Partido con los datos recuperados
+                // Crear el objeto Partido con los datos recuperados
                 Partido p = new Partido(nombre, votos, siglas);
                 resultado.add(p);
             }
